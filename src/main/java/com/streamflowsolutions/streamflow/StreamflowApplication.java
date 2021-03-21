8c2983d8beed5177e4f3ced7f2ac1bdf6aa45c56
Yehor Bublyk <yehor.bk@gmail.com>
2021-03-21 13:27:48 +0200

    project added

---
+++ src/main/java/com/streamflowsolutions/streamflow/StreamflowApplication.java
@@ -0,0 +1,13 @@
+package com.streamflowsolutions.streamflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamflowApplication.class, args);
+	}
+
+}
