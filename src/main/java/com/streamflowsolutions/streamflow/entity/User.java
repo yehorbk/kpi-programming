809b56da2994a5d3a70d21be705b9b199433e9ba
Yehor Bublyk <yehor.bk@gmail.com>
2021-03-21 14:00:43 +0200

    user entity created

---
+++ src/main/java/com/streamflowsolutions/streamflow/entity/User.java
@@ -0,0 +1,40 @@
+package com.streamflowsolutions.streamflow.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = { "password" })
+@ToString(exclude = { "password" })
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+            
+    @Column(name = "password", nullable = false)
+    private String password;
+    
+    /*
+        TODO: implement more fields
+    */
+    
+}
