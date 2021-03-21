1d06e5f0a80eb8bc11e9092c3906f52b65736a01
Yehor Bublyk <yehor.bk@gmail.com>
2021-03-21 13:33:13 +0200

    security config disalbled

--- src/main/java/com/streamflowsolutions/streamflow/StreamflowApplication.java
+++ src/main/java/com/streamflowsolutions/streamflow/StreamflowApplication.java
@@ -2,12 +2,13 @@ package com.streamflowsolutions.streamflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class StreamflowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamflowApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(StreamflowApplication.class, args);
+    }
 
 }
