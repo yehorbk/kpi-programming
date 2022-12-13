64d6bee5a8899db3dc6fbc03d75636ebe57f3088
yehorbk <yehor.bk@gmail.com>
2022-12-13 18:31:50 +0200

    software-security/lab1: project added

---
+++ Software-Security/lab1/forms_auth/index.js
@@ -0,0 +1,144 @@
+const uuid = require('uuid');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const onFinished = require('on-finished');
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = 3000;
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const SESSION_KEY = 'session';
+
+class Session {
+    #sessions = {}
+
+    constructor() {
+        try {
+            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
+            this.#sessions = JSON.parse(this.#sessions.trim());
+
+            console.log(this.#sessions);
+        } catch(e) {
+            this.#sessions = {};
+        }
+    }
+
+    #storeSessions() {
+        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
+    }
+
+    set(key, value) {
+        if (!value) {
+            value = {};
+        }
+        this.#sessions[key] = value;
+        this.#storeSessions();
+    }
+
+    get(key) {
+        return this.#sessions[key];
+    }
+
+    init(res) {
+        const sessionId = uuid.v4();
+        res.set('Set-Cookie', `${SESSION_KEY}=${sessionId}; HttpOnly`);
+        this.set(sessionId);
+
+        return sessionId;
+    }
+
+    destroy(req, res) {
+        const sessionId = req.sessionId;
+        delete this.#sessions[sessionId];
+        this.#storeSessions();
+        res.set('Set-Cookie', `${SESSION_KEY}=; HttpOnly`);
+    }
+}
+
+const sessions = new Session();
+
+app.use((req, res, next) => {
+    let currentSession = {};
+    let sessionId;
+
+    if (req.cookies[SESSION_KEY]) {
+        sessionId = req.cookies[SESSION_KEY];
+        currentSession = sessions.get(sessionId);
+        if (!currentSession) {
+            currentSession = {};
+            sessionId = sessions.init(res);
+        }
+    } else {
+        sessionId = sessions.init(res);
+    }
+
+    req.session = currentSession;
+    req.sessionId = sessionId;
+
+    onFinished(req, () => {
+        const currentSession = req.session;
+        const sessionId = req.sessionId;
+        sessions.set(sessionId, currentSession);
+    });
+
+    next();
+});
+
+app.get('/', (req, res) => {
+    console.log(req.session);
+
+    if (req.session.username) {
+        return res.json({
+            username: req.session.username,
+            logout: 'http://localhost:3000/logout'
+        })
+    }
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.get('/logout', (req, res) => {
+    sessions.destroy(req, res);
+    res.redirect('/');
+});
+
+const users = [
+    {
+        login: 'Login',
+        password: 'Password',
+        username: 'Username',
+    },
+    {
+        login: 'Login1',
+        password: 'Password1',
+        username: 'Username1',
+    }
+]
+
+app.post('/api/login', (req, res) => {
+    const { login, password } = req.body;
+
+    const user = users.find((user) => {
+        if (user.login == login && user.password == password) {
+            return true;
+        }
+        return false
+    });
+
+    if (user) {
+        req.session.username = user.username;
+        req.session.login = user.login;
+
+        res.json({ username: login });
+    }
+
+    res.status(401).send();
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ Software-Security/lab1/token_auth/index.js
@@ -0,0 +1,137 @@
+const uuid = require('uuid');
+const express = require('express');
+const onFinished = require('on-finished');
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = 3000;
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const SESSION_KEY = 'Authorization';
+
+class Session {
+    #sessions = {}
+
+    constructor() {
+        try {
+            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
+            this.#sessions = JSON.parse(this.#sessions.trim());
+
+            console.log(this.#sessions);
+        } catch(e) {
+            this.#sessions = {};
+        }
+    }
+
+    #storeSessions() {
+        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
+    }
+
+    set(key, value) {
+        if (!value) {
+            value = {};
+        }
+        this.#sessions[key] = value;
+        this.#storeSessions();
+    }
+
+    get(key) {
+        return this.#sessions[key];
+    }
+
+    init(res) {
+        const sessionId = uuid.v4();
+        this.set(sessionId);
+
+        return sessionId;
+    }
+
+    destroy(req, res) {
+        const sessionId = req.sessionId;
+        delete this.#sessions[sessionId];
+        this.#storeSessions();
+    }
+}
+
+const sessions = new Session();
+
+app.use((req, res, next) => {
+    let currentSession = {};
+    let sessionId = req.get(SESSION_KEY);
+
+    if (sessionId) {
+        currentSession = sessions.get(sessionId);
+        if (!currentSession) {
+            currentSession = {};
+            sessionId = sessions.init(res);
+        }
+    } else {
+        sessionId = sessions.init(res);
+    }
+
+    req.session = currentSession;
+    req.sessionId = sessionId;
+
+    onFinished(req, () => {
+        const currentSession = req.session;
+        const sessionId = req.sessionId;
+        sessions.set(sessionId, currentSession);
+    });
+
+    next();
+});
+
+app.get('/', (req, res) => {
+    if (req.session.username) {
+        return res.json({
+            username: req.session.username,
+            logout: 'http://localhost:3000/logout'
+        })
+    }
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.get('/logout', (req, res) => {
+    sessions.destroy(req, res);
+    res.redirect('/');
+});
+
+const users = [
+    {
+        login: 'Login',
+        password: 'Password',
+        username: 'Username',
+    },
+    {
+        login: 'Login1',
+        password: 'Password1',
+        username: 'Username1',
+    }
+]
+
+app.post('/api/login', (req, res) => {
+    const { login, password } = req.body;
+
+    const user = users.find((user) => {
+        if (user.login == login && user.password == password) {
+            return true;
+        }
+        return false
+    });
+
+    if (user) {
+        req.session.username = user.username;
+        req.session.login = user.login;
+
+        res.json({ token: req.sessionId });
+    }
+
+    res.status(401).send();
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
